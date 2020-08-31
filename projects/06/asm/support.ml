e642d37c5636f13cfcbde27d53ab78cace64fc7e
ghassheee <ghassheee@gmail.com>
2020-09-01 03:17:19 +0900

    Updated

--- projects/06/asm/support.ml
+++ projects/06/asm/support.ml
@@ -8,9 +8,9 @@ type 'a withinfo    = {i: info ; v: 'a}
 let createInfo l    = L(l) 
 
 
-    exception LabelNameDuplicated 
-    type tbl = (string,int) Hashtbl.t
-    let add tbl s i = 
+exception LabelNameDuplicated 
+type tbl = (string,int) Hashtbl.t
+let add tbl s i = 
         try Hashtbl.find tbl s ; raise LabelNameDuplicated 
         with | LabelNameDuplicated -> raise LabelNameDuplicated 
              | e -> Hashtbl.add tbl s i 
